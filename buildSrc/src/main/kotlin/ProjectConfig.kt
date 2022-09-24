8a875ccf9cc15acd6a70d91c845a98806566989c
KazemM <76521159+kazemcodes@users.noreply.github.com>
2022-09-24 14:37:39 +0330

    Release v0.1.31

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -3,7 +3,7 @@ object ProjectConfig {
     const val targetSdk = 29
     const val compileSdk = 33
     const val ndk = "21.3.6528147"
-    const val versionName = "0.1.30"
-    const val versionCode = 37
+    const val versionName = "0.1.31"
+    const val versionCode = 38
     const val applicationId = "ir.kazemcodes.infinityreader"
 }
