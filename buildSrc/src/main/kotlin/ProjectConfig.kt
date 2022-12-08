d2895d67268fb30049b4b7dce03bda437c819616
Kazem <e.kazemmoridi@gmail.com>
2022-12-08 12:48:04 +0330

    Release 0.1.34

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -3,7 +3,7 @@ object ProjectConfig {
     const val targetSdk = 29
     const val compileSdk = 33
     const val ndk = "21.3.6528147"
-    const val versionName = "0.1.33"
-    const val versionCode = 40
+    const val versionName = "0.1.34"
+    const val versionCode = 41
     const val applicationId = "ir.kazemcodes.infinityreader"
 }
