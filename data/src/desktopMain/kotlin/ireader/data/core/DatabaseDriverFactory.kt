add791a5f0263bad9b6d294a8505ff0f10f39792
kazem <kazem.codes@gmail.com>
2023-04-12 10:43:12 +0330

    fix db file not saved in desktop

--- data/src/desktopMain/kotlin/ireader/data/core/DatabaseDriverFactory.kt
+++ data/src/desktopMain/kotlin/ireader/data/core/DatabaseDriverFactory.kt
@@ -3,12 +3,19 @@ package ireader.data.core
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import ir.kazemcodes.infinityreader.Database
+import ireader.core.storage.AppDir
+import java.io.File
 import java.util.*
 
 actual class DatabaseDriverFactory {
     actual fun create(): SqlDriver {
+        val dbDir = File(AppDir, "database/")
+        if (!dbDir.exists()) {
+            AppDir.mkdirs()
+        }
+        val dbFile = File(dbDir, "ireader.db")
         val driver = JdbcSqliteDriver(
-            url = JdbcSqliteDriver.IN_MEMORY,
+            url = JdbcSqliteDriver.IN_MEMORY.plus(dbFile.absolutePath),
             properties = Properties().apply {
                 put("foreign_keys", "true")
             }
