d0ffc290e50682be1836b35d9b7bc7fd73648a77
kazem <kazem.codes@gmail.com>
2024-04-25 23:34:52 +0330

    Release 0.1.38

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -5,8 +5,8 @@ object ProjectConfig {
     const val targetSdk = 29
     const val compileSdk = 34
     const val ndk = "21.3.6528147"
-    const val versionName = "0.1.37"
-    const val versionCode = 43
+    const val versionName = "0.1.38"
+    const val versionCode = 44
     const val applicationId = "ir.kazemcodes.infinityreader"
 
     val desktopJvmTarget = JavaVersion.VERSION_17
