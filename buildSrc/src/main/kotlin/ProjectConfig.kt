49999883ca039a1bccd073e1e42c20f8acc0f957
Kazem <kazem.codes@gmail.com>
2024-10-10 13:02:47 +0330

    update gradle

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -3,13 +3,13 @@ import org.gradle.api.JavaVersion
 object ProjectConfig {
     const val minSdk = 21
     const val targetSdk = 29
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val ndk = "21.3.6528147"
     const val versionName = "0.1.38"
     const val versionCode = 45
     const val applicationId = "ir.kazemcodes.infinityreader"
 
-    val desktopJvmTarget = JavaVersion.VERSION_17
-    val androidJvmTarget = JavaVersion.VERSION_17
+    val desktopJvmTarget = JavaVersion.VERSION_21
+    val androidJvmTarget = JavaVersion.VERSION_21
     val toolChain = 11
 }
