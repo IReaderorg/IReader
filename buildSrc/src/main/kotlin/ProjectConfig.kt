ab5e21fc2ab6444830408179ddc123989afff087
Kazem <kazem.codes@gmail.com>
2025-04-04 22:55:17 +0330

    update versions

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object ProjectConfig {
     const val minSdk = 21
-    const val targetSdk = 29
+    const val targetSdk = 35
     const val compileSdk = 35
     const val ndk = "21.3.6528147"
     const val versionName = "0.1.39"
