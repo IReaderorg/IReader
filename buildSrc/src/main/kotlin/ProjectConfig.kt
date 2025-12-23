e9a5a9c9f1101263348a726645b1fc900676fa6e
kazem <kazem.codes@gmail.com>
2025-12-24 00:01:14 +0330

    update versions

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -5,8 +5,8 @@ object ProjectConfig {
     const val targetSdk = 35
     const val compileSdk = 36
     const val ndk = "21.3.6528147"
-    const val versionName = "2.0.7"  // Changed from 0.1.44 to meet DMG packaging requirements (MAJOR > 0)
-    const val versionCode = 61
+    const val versionName = "2.0.8"  // Changed from 0.1.44 to meet DMG packaging requirements (MAJOR > 0)
+    const val versionCode = 62
     const val applicationId = "ir.kazemcodes.infinityreader"
 
     val desktopJvmTarget = JavaVersion.VERSION_21
