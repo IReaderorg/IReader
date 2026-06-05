01323ab5afcb20603e380a345554d1b541267a01
Kazem <kazem.codes@gmail.com>
2026-06-05 11:06:56 +0330

    fix supbase not athenticating

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -232,30 +232,31 @@ buildkonfig {
     
     defaultConfigs {
         // Supabase Configuration - 7 Projects
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_AUTH_URL", "\"${getConfigProperty("SUPABASE_AUTH_URL", "supabase.auth.url")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_AUTH_KEY", "\"${getConfigProperty("SUPABASE_AUTH_KEY", "supabase.auth.key")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_READING_URL", "\"${getConfigProperty("SUPABASE_READING_URL", "supabase.reading.url")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_READING_KEY", "\"${getConfigProperty("SUPABASE_READING_KEY", "supabase.reading.key")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_LIBRARY_URL", "\"${getConfigProperty("SUPABASE_LIBRARY_URL", "supabase.library.url")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_LIBRARY_KEY", "\"${getConfigProperty("SUPABASE_LIBRARY_KEY", "supabase.library.key")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_BOOK_REVIEWS_URL", "\"${getConfigProperty("SUPABASE_BOOK_REVIEWS_URL", "supabase.book_reviews.url")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_BOOK_REVIEWS_KEY", "\"${getConfigProperty("SUPABASE_BOOK_REVIEWS_KEY", "supabase.book_reviews.key")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_CHAPTER_REVIEWS_URL", "\"${getConfigProperty("SUPABASE_CHAPTER_REVIEWS_URL", "supabase.chapter_reviews.url")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_CHAPTER_REVIEWS_KEY", "\"${getConfigProperty("SUPABASE_CHAPTER_REVIEWS_KEY", "supabase.chapter_reviews.key")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_BADGES_URL", "\"${getConfigProperty("SUPABASE_BADGES_URL", "supabase.badges.url")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_BADGES_KEY", "\"${getConfigProperty("SUPABASE_BADGES_KEY", "supabase.badges.key")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_ANALYTICS_URL", "\"${getConfigProperty("SUPABASE_ANALYTICS_URL", "supabase.analytics.url")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_ANALYTICS_KEY", "\"${getConfigProperty("SUPABASE_ANALYTICS_KEY", "supabase.analytics.key")}\"")
+        // Note: buildkonfig automatically adds quotes for STRING type, so we don't need escaped quotes
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_AUTH_URL", getConfigProperty("SUPABASE_AUTH_URL", "supabase.auth.url"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_AUTH_KEY", getConfigProperty("SUPABASE_AUTH_KEY", "supabase.auth.key"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_READING_URL", getConfigProperty("SUPABASE_READING_URL", "supabase.reading.url"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_READING_KEY", getConfigProperty("SUPABASE_READING_KEY", "supabase.reading.key"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_LIBRARY_URL", getConfigProperty("SUPABASE_LIBRARY_URL", "supabase.library.url"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_LIBRARY_KEY", getConfigProperty("SUPABASE_LIBRARY_KEY", "supabase.library.key"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_BOOK_REVIEWS_URL", getConfigProperty("SUPABASE_BOOK_REVIEWS_URL", "supabase.book_reviews.url"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_BOOK_REVIEWS_KEY", getConfigProperty("SUPABASE_BOOK_REVIEWS_KEY", "supabase.book_reviews.key"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_CHAPTER_REVIEWS_URL", getConfigProperty("SUPABASE_CHAPTER_REVIEWS_URL", "supabase.chapter_reviews.url"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_CHAPTER_REVIEWS_KEY", getConfigProperty("SUPABASE_CHAPTER_REVIEWS_KEY", "supabase.chapter_reviews.key"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_BADGES_URL", getConfigProperty("SUPABASE_BADGES_URL", "supabase.badges.url"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_BADGES_KEY", getConfigProperty("SUPABASE_BADGES_KEY", "supabase.badges.key"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_ANALYTICS_URL", getConfigProperty("SUPABASE_ANALYTICS_URL", "supabase.analytics.url"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "SUPABASE_ANALYTICS_KEY", getConfigProperty("SUPABASE_ANALYTICS_KEY", "supabase.analytics.key"))
         
         // Cloudflare D1 + R2 Community Translations Configuration
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_CLOUDFLARE_ACCOUNT_ID", "\"${getConfigProperty("COMMUNITY_CLOUDFLARE_ACCOUNT_ID", "community.cloudflare.accountId")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_CLOUDFLARE_API_TOKEN", "\"${getConfigProperty("COMMUNITY_CLOUDFLARE_API_TOKEN", "community.cloudflare.apiToken")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_D1_DATABASE_ID", "\"${getConfigProperty("COMMUNITY_D1_DATABASE_ID", "community.d1.databaseId")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_R2_BUCKET_NAME", "\"${getConfigProperty("COMMUNITY_R2_BUCKET_NAME", "community.r2.bucketName")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_R2_PUBLIC_URL", "\"${getConfigProperty("COMMUNITY_R2_PUBLIC_URL", "community.r2.publicUrl")}\"")
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_CLOUDFLARE_ACCOUNT_ID", getConfigProperty("COMMUNITY_CLOUDFLARE_ACCOUNT_ID", "community.cloudflare.accountId"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_CLOUDFLARE_API_TOKEN", getConfigProperty("COMMUNITY_CLOUDFLARE_API_TOKEN", "community.cloudflare.apiToken"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_D1_DATABASE_ID", getConfigProperty("COMMUNITY_D1_DATABASE_ID", "community.d1.databaseId"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_R2_BUCKET_NAME", getConfigProperty("COMMUNITY_R2_BUCKET_NAME", "community.r2.bucketName"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "COMMUNITY_R2_PUBLIC_URL", getConfigProperty("COMMUNITY_R2_PUBLIC_URL", "community.r2.publicUrl"))
         
         // Discord Webhooks Configuration
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "DISCORD_CHARACTER_ART_WEBHOOK_URL", "\"${getConfigProperty("DISCORD_CHARACTER_ART_WEBHOOK_URL", "discord.characterArt.webhookUrl")}\"")
-        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "DISCORD_QUOTE_WEBHOOK_URL", "\"${getConfigProperty("DISCORD_QUOTE_WEBHOOK_URL", "discord.quote.webhookUrl")}\"")
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "DISCORD_CHARACTER_ART_WEBHOOK_URL", getConfigProperty("DISCORD_CHARACTER_ART_WEBHOOK_URL", "discord.characterArt.webhookUrl"))
+        buildConfigField(com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING, "DISCORD_QUOTE_WEBHOOK_URL", getConfigProperty("DISCORD_QUOTE_WEBHOOK_URL", "discord.quote.webhookUrl"))
     }
 }
