8453c1c2fb97102becfdeda1102f1fce74cb73fc
Kazem <kazem.codes@gmail.com>
2026-06-29 18:20:31 +0330

    fix

--- source-api/src/desktopMain/kotlin/eu/kanade/tachiyomi/source/model/FilterList.kt
+++ source-api/src/desktopMain/kotlin/eu/kanade/tachiyomi/source/model/FilterList.kt
@@ -1,8 +1,5 @@
 package eu.kanade.tachiyomi.source.model
 
-import androidx.compose.runtime.Stable
-
-@Stable
 data class FilterList(val list: List<Filter<*>>) : List<Filter<*>> by list {
 
     constructor(vararg fs: Filter<*>) : this(if (fs.isNotEmpty()) fs.asList() else emptyList())
